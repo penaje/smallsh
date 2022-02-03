edc8e894e1a4b074035091872c0edd22d3f65f75
penaje <penaje@oregonstate.edu>
2022-02-03 13:46:33 -0800

    taking input works

--- smallsh.c
+++ smallsh.c
@@ -13,55 +13,138 @@
 struct commandStruct {
     char* command;
     char* args[514];
-    char* iRedirect;
-    char* oRedirect;
+    char* inpRedir;
+    char* outpRedir;
     bool background;
 };
 
-struct commandStruct* getInput(char* currLine)
+bool fgFlag = false;
+
+struct commandStruct* parseInput(char* currLine, bool fgFlag)
 {
     struct commandStruct* newCommand = malloc(sizeof(struct commandStruct));
 
     //for use with strtok_r
     char* saveptr;
 
-    //the token will be the command
-    char* token = strtok_r(currLine, " ", &saveptr);
+    //the first token will be the command
+    char* token = strtok_r(currLine, " \n", &saveptr);
 
     //If there is no command we return NULL
-    if (token != NULL)
+    if (token == NULL)
     {
-       newCommand->command = calloc(strlen(token) + 1, sizeof(char));
-       strcpy(newCommand->command, token);
+        return NULL;
     }
     else
     {
-        return token;
+        newCommand->command = calloc(strlen(token) + 1, sizeof(char));
+        strcpy(newCommand->command, token);
     }  
 
+    //advance the token
+    token = strtok_r(NULL, " \n", &saveptr);
+
+    //if no args or redirect or BG then we just return the command
+    if (token == NULL)
+    {
+        return newCommand;
+    }
+
     //Now we get the arguments
-    token = strtok_r(NULL, " ", &saveptr);
     int i = 0;
     newCommand->args[0] = calloc(strlen(token) + 1, sizeof(char));
-    strcpy(newCommand->args[0], token);
-    
+    strcpy(newCommand->args[0], token); 
+     
 
     while ((token != NULL) && (strcmp(token, "<") != 0) && (strcmp(token, ">") != 0) && (strcmp(token, "&") != 0))
     {
         newCommand->args[i] = calloc(strlen(token) + 1, sizeof(char));
         strcpy(newCommand->args[i], token);
         i++;
-        token = strtok_r(NULL, " ", &saveptr);
+        token = strtok_r(NULL, " \n", &saveptr);
     }
 
     //ends the array with a NULL pointer
     newCommand->args[i] = malloc(sizeof(char));
     newCommand->args[i] = NULL;
 
+    while (token != NULL)
+    {
+        if (strcmp(token, "<") == 0)
+        {
+            token = strtok_r(NULL, " \n", &saveptr);
+            newCommand->inpRedir = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(newCommand->inpRedir, token);
+        }
+        else if (strcmp(token, ">") == 0)
+        {
+            token = strtok_r(NULL, " \n", &saveptr);
+            newCommand->outpRedir = calloc(strlen(token) + 1, sizeof(char));
+            strcpy(newCommand->outpRedir, token);
+        }
+        else if ((strcmp(token, "&") == 0) && (fgFlag == false))
+        {
+            newCommand->background = true;
+        }
 
+        token = strtok_r(NULL, " \n", &saveptr);
+    }
 
+    return newCommand;
 
+}
 
+char* getInput() 
+{
+    // Displays the command line prompt
+    printf(": ");
+    fflush(stdout);
 
+    // Stores input from stdin up to 2048 characters or until a new line character
+    char* inputText = calloc(2049, sizeof(char));
+    fgets(inputText, 2048, stdin);
+
+    return inputText;
+}
+
+void printCommand(struct commandStruct* aCommand) {
+    printf("command: %s | inputRedir: %s | OutputRedir: %s | Background: %d | ", 
+        aCommand->command,
+        aCommand->inpRedir,
+        aCommand->outpRedir,
+        aCommand->background);
+
+    int i = 0;
+    while (aCommand->args[i] != NULL)
+    {
+        printf("args[%d]: %s ", i, aCommand->args[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    printf("$ smallsh\n");
+    fflush(stdout);
+
+    char* input;
+    struct commandStruct* commandLine;
+
+    input = getInput();
+
+    commandLine = parseInput(input, fgFlag);
+
+    if (commandLine != NULL)
+    {
+        printCommand(commandLine);
+    }
+    else
+    {
+        input = getInput();
+    }
+    
+
+    return 0;
 
 }
